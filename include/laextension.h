b5a076738df92f375e52d23af48d4db5c9caa167
SiddhartaL <siddharta_laloux@brown.edu>
2026-05-07 16:22:49 -0400

    integration of laextension and cmsis-dsp

--- include/laextension.h
+++ include/laextension.h
@@ -1,8 +1,10 @@
 #ifndef LAEXTENSION
 #define LAEXTENSION
-void cross(float* A, float* B, float* C);
-float min_arr(float* A, int elements);
-float max_arr(float* A, int elements);
-float trace(float* A, int side);
-float l2_norm(float* A, int size);
+#include "arm_math.h"
+
+void cross(float32_t *A, float32_t *B, float32_t *C); 
+float32_t min_arr(float32_t* A, int elements);
+float32_t max_arr(float32_t* A, int elements);
+float33_t trace(arm_matrix_instance_f32* A); 
+float32_t l2_norm(float32_t* A, int size);
 #endif
--- src/laextension.c
+++ src/laextension.c
@@ -1,50 +1,119 @@
 #include "include/laextension.h"
+#include "Include/arm_math_types.h"
+#include "Include/dsp/matrix_functions.h"
+#include "Include/dsp/statistics_functions.h"
 #include "math.h"
-/*Cross Product for 2 1x3 vectors*/
-void cross(float* A, float* B, float* C){
+#include "arm_math.h"
+
+/**
+ * \fn cross
+ * 
+ * \brief Compute the cross product of two 1x3 vectors
+ *
+ * \param[in] A: First vector
+ * \param[in] B: Second vector
+ * \param[out] C: Resultant vector
+ * 
+ * \return void
+ */
+void cross(float32_t *A, float32_t *B, float32_t *C){
     C[0] = A[1]*B[2] - A[2]*B[1];
     C[1] = A[2]*B[0] - A[0]*B[2];
     C[2] = A[0]*B[1] - A[1]*B[0];
 }
+
 /*Max*/
-float max_arr(float* A, int elements){
+/**
+ * \fn max_arr
+ * 
+ * \brief Find the maximum value in an array
+ * 
+ * \param[in] A: Array of floats
+ * \param[in] elements: Number of elements in the array
+ * 
+ * \return float32_t, maximum value in the array
+ */
+inline float32_t max_arr(float32_t *A, int elements){
+    
+    float32_t max; 
+
     if(elements == 0){
         return 0;
+    } else {
+        arm_max_no_idx_f32(A, elements, &max);
     }
-    else{
-        float max_found = A[0];
-        for(int i = 0; i<elements; i++){
-            max_found = fmaxf(max_found, A[i]);
-        }
-        return max_found;
-    }
+    return max;
 }
-/*Mix*/
-float min_arr(float* A, int elements){
+
+
+/**
+ * \fn min_arr 
+ * 
+ * \brief returns the minimum value in a flat array
+ * 
+ * \param[in] A: Array of floats
+ * \param[in] elements: Number of elements in the array
+ * 
+ * \return float32_t, minimum value in the array 
+ */
+inline float32_t min_arr(float32_t *A, int elements){
+
+    float32_t min;
+
     if(elements == 0){
         return 0;
+    } else {
+        arm_min_no_idx_f32(A, elements, &min);
     }
-    else{
-        float min_found = A[0];
-        for(int i = 0; i<elements; i++){
-            min_found = fminf(min_found, A[i]);
-        }
-        return min_found;
-    }
+    return min;
 }
-/*Trace*/
-float trace(float* A, int sz){
-    float n= 0;
-    for(int i = 0; i<sz; i++){
-        n += A[i * sz + i];
+
+
+/**
+ * \fn trace
+ * 
+ * \brief Compute the trace of a square matrix
+ * 
+ * \param[in] A: Square matrix
+ * \param[in] sz: Size of the matrix
+ * 
+ * \return float, trace of the matrix
+ */
+float32_t trace(arm_matrix_instance_f32* A){
+    
+    if (A->numRows != A->numCols){
+        return 0;
     }
-    return n;
+    
+    int sz = A->numRows;
+    float32_t *data = A->pData;
+    float32_t trace = 0; 
+
+    for (int i = 0; i < sz; i++) {
+        trace += data[i * sz + i];
+    }
+    
+    return trace;
 }
-/*L2Norm*/
-float l2_norm(float* A, int n){
-    float x = 0;
-    for (int i = 0; i<n; i++){
-        x += pow(A[i], 2);
-    }
-    return sqrtf(x);
+
+/**
+ * \fn l2_norm
+ * 
+ * \brief Compute the L2 norm of a vector
+ * 
+ * \param[in] A: Vector
+ * \param[in] n: Number of elements in the vector
+ * 
+ * \return float, L2 norm of the vector
+ */
+inline float32_t l2_norm(float32_t *A, int n){
+    
+    float32_t sum_squares;
+    float32_t norm;
+
+    arm_power_f32(A, n, &sum_squares);
+   
+    arm_sqrt_f32(sum_squares, &norm);
+    
+    return norm;
 }
