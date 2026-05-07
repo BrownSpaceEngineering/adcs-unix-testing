5b382c6a2781ca7985e169cb4b738929dca09c08
SiddhartaL <siddharta_laloux@brown.edu>
2026-05-07 17:41:33 -0400

    integration of moments2currents with cmsis dsp

--- include/moments2currents.h
+++ include/moments2currents.h
@@ -1,4 +1,4 @@
 #ifndef MOMENTS2CURRENTS
 #define MOMENTS2CURRENTS
-void moment2current3axis(float* m, float* Imax, float* I_out);
+void moment2current3axis(float32_t *m, float32_t *Imax, float32_t *I_out);
 #endif
--- src/moments2currents.c
+++ src/moments2currents.c
@@ -1,34 +1,37 @@
 #include <math.h>
+#include "arm_math.h"
 
 /**
- * moment2current3axis
- * Converts desired moment into currents for 3 orthogonal magnetorquers
+ * \fn moment2current3axis
+ * \brief Converts desired moment into currents for 3 orthogonal magnetorquers
  *
  * Inputs:
- *   m       : 3-element array, magnetic moment [mx, my, mz] [A·m2]
- *   Imax    : 3-element array of current limits [A] (use -1 at an axis if no current limit on that axis)
- *   I_out   : 3-element output array for coil currents [Ix, Iy, Iz] [A]
+ *  \param[in] m       : 3-element array, magnetic moment [mx, my, mz] [A·m2]
+ *  \param[in] Imax    : 3-element array of current limits [A] (use -1 at an axis if no current limit on that axis)
+ *  \param[out] I_out   : 3-element output array for coil currents [Ix, Iy, Iz] [A]
  */
-void moment2current3axis(float* m, float* Imax, float* I_out)
-{
+void moment2current3axis(float32_t *m, float32_t *Imax, float32_t *I_out) {
+
     /* Hard-coded parameters (to be measured/calculated)
      *   n    : turns per coil
      *   A    : coil areas [m2]
      *   L, r : rod geometry (ignored if mu_r == 1)
      *   mu_r : relative permeability (1 for air-core)
      */
-    float n[3]    = {1.0, 1.0, 1.0};
-    float A[3]    = {1.0, 1.0, 1.0};
-    float L[3]    = {1.0, 1.0, 1.0};
-    float r[3]    = {1.0, 1.0, 1.0};
-    float mu_r[3] = {1.0, 1.0, 1.0};
+    float32_t n[3]    = {1.0, 1.0, 1.0};
+    float32_t A[3]    = {1.0, 1.0, 1.0};
+    float32_t L[3]    = {1.0, 1.0, 1.0};
+    float32_t r[3]    = {1.0, 1.0, 1.0};
+    float32_t mu_r[3] = {1.0, 1.0, 1.0};
 
     /* Compute core amplification factors G */
-    float G[3];
+    float32_t G[3];
+    float32_t ratio, Nd;
+
     for (int i = 0; i < 3; i++) {
         if (mu_r[i] > 1.0) {
-            float ratio = L[i] / r[i];
-            float Nd    = 4.0 * (log(ratio) - 1.0) / (ratio * ratio - 4.0 * logf(ratio));
+            ratio = L[i] / r[i];
+            Nd    = 4.0 * (logf(ratio) - 1.0) / (ratio * ratio - 4.0 * logf(ratio));
 
             /* Core amplification */
             G[i] = 1.0 + (mu_r[i] - 1.0) / (1.0 + (mu_r[i] - 1.0) * Nd);
